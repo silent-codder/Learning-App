af85acc67ec28ac97c662c39df266f12c0440b9d
Ajinkya Shinde <55943387+silent-codder@users.noreply.github.com>
2020-07-12 17:46:04 +0530

    animal info update 30%

--- app/src/main/java/com/silentcodder/learningapp/Animal.java
+++ app/src/main/java/com/silentcodder/learningapp/Animal.java
@@ -2,13 +2,42 @@ package com.silentcodder.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Animal extends AppCompatActivity {
+
+    LinearLayout tiger,lion,bear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal);
+
+        tiger = (LinearLayout) findViewById(R.id.btn_readMore_tiger);
+        lion = (LinearLayout) findViewById(R.id.btn_readMore_lion);
+        bear = (LinearLayout) findViewById(R.id.btn_readMore_bear);
+
+        tiger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Animal.this, Tiger_Info.class));
+            }
+        });
+        lion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Animal.this, Lion_Info.class));
+            }
+        });
+        bear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Animal.this, Bear_Info.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/silentcodder/learningapp/Bear_Info.java
@@ -0,0 +1,14 @@
+package com.silentcodder.learningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bear_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bear__info);
+    }
+}

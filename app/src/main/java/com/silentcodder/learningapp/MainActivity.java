e1574cb218417e02f0bd801c3c17940ceba385bb
Ajinkya Shinde <55943387+silent-codder@users.noreply.github.com>
2020-07-12 14:12:25 +0530

    animal activity create

--- app/src/main/java/com/silentcodder/learningapp/MainActivity.java
+++ app/src/main/java/com/silentcodder/learningapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.silentcodder.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout b3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b3 = (LinearLayout) findViewById(R.id.b3);
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Animal.class));
+            }
+        });
     }
 }

e70890a3bbc47226a6d8e1fcbf21c54fbd90aaf0
Ajinkya Shinde <55943387+silent-codder@users.noreply.github.com>
2020-07-17 13:05:46 +0530

    add dialog box

---
+++ app/src/main/java/com/silentcodder/learningapp/Mulakshare.java
@@ -0,0 +1,13 @@
+package com.silentcodder.learningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mulakshare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mulakshare);
+    }
+}
